2434e48e1629d622c63c3568fd1c3df32c875c28
jahanna4 <61722728+jahanna4@users.noreply.github.com>
2020-06-24 23:17:28 -0400

    updated
    updated

--- StarterCode/app.js
+++ StarterCode/app.js
@@ -6,6 +6,13 @@ d3.json("static/js/samples.json").then(function(rawdata) {
     var data = rawdata;
     var meta = data.metadata;
     var samples = data.samples;
+    var names = data.names;
+    console.log(meta);
+
+    // dataselect = d3.select("#selDataset");
+    // dataselect.append("ul");
+    // dataselect.select("ul").selectAll("li").data(names).enter().append("li").text(names);
+
     var otuids = [];
     var otulabels = [];
     var sampvals = [];
@@ -14,19 +21,31 @@ d3.json("static/js/samples.json").then(function(rawdata) {
         // for(var x = 0; x<samples[i].otu_ids; i++) {
         //     console.log(samples[3].otu_ids[1]);
         // }
-        otuids=samples[8].otu_ids;
-        otulabels=samples[8].otu_labels;
-        sampvals=samples[8].sample_values;
-        id = meta[8].id;
-        ethnicity = meta[8].ethnicity;
-        gender = meta[8].gender;
-        age = meta[8].age;
-        loc = (meta[8].location).replace("/",",");
-        bbtype = meta[8].bbtype;
-        wfreq = meta[8].wfreq;
+       var otuids=samples[5].otu_ids;
+       var otulabels=samples[5].otu_labels;
+       var sampvals=samples[5].sample_values;
+
+       var id = meta[5].id;
+       var ethnicity = meta[5].ethnicity;
+       var gender = meta[5].gender;
+       var age = meta[5].age;
+       var loc = (meta[5].location).replace("/",",");
+       var bbtype = meta[5].bbtype;
+       var wfreq = meta[5].wfreq;
+       var demdata = [id, ethnicity, gender, age, loc, bbtype, wfreq];
+       console.log(demdata);
         // console.log(loc);
-        d3.select("#selDataset")
+        // var metadisp = d3.select("#metadata");
+        // meta.forEach((meta) => {
+        //     var newrow = metadisp.append("ul");
+        //     Object.entries(meta).forEach(([key,value]) => {
+        //         var newcell = newrow.append("li");
+        //         newcell.text(value);
+        //     });
+        // });
     // }
+
+
     function initbar() {
         var initBarTrace = {
             x: otuids,
@@ -61,27 +80,19 @@ d3.json("static/js/samples.json").then(function(rawdata) {
         Plotly.newPlot("bubble", bubbleData, bubbleLayout);
     };
     initbubble();
-    
+
+    function initgauge() {
+        var gaugedata = [ {
+            domain: {x:[0,9], y:[0,9]},
+            value: wfreq,
+            title: {text: "Belly Button Washing Frequency"},
+            type: "indicator",
+            mode: "gauge+number"
+        }
+        ];
+        var gaugelayout = {width: 450, height: 500};
+        Plotly.newPlot("gauge", gaugedata, gaugelayout);
+    };
+    initgauge();
+
 });
-// console.log(otuids);
-// testids = [2419, 944, 1795];
-// testvals = [8, 4, 3];
-// testlabels = ["Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Actinobacteria;Actinobacteria;Actinomycetales;Corynebacteriaceae;Corynebacterium", "Bacteria;Firmicutes;Bacilli;Bacillales;Staphylococcaceae;Staphylococcus"];
-
-// function init() {
-//     initTrace = {
-//         x: `OTU ${otuids}`,
-//         y: sampvals,
-//         type: "bar",
-//         text: otulabels
-//     };
-
-//     var data = [initTrace];
-//     var layout = {
-//         title: "Top OTU's"
-//     };
-//     Plotly.newPlot("bar", data, layout);
-// }
-
-// init();
-
